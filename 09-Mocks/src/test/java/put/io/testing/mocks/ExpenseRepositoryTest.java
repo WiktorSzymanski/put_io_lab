ae65205002991ea9b17e76a6cd31306e75d1b663
DarkVikos <darkvikos@gmail.com>
2022-12-15 16:59:53 +0100

    innit 09-Mocks

---
+++ 09-Mocks/src/test/java/put/io/testing/mocks/ExpenseRepositoryTest.java
@@ -0,0 +1,19 @@
+package put.io.testing.mocks;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+
+import put.io.students.fancylibrary.database.FancyDatabase;
+import put.io.students.fancylibrary.database.IFancyDatabase;
+
+public class ExpenseRepositoryTest {
+
+    @Test
+    void loadExpenses() {
+    }
+}
